88c120d655b96112bbdbe7278f564cf0f0423174
erush4 <ethan.t.rushforth@gmail.com>
2024-11-29 15:04:18 -0800

    Create Sign-in component
    entirely working w/ react`

--- src/sign-in/sign-in.jsx
+++ src/sign-in/sign-in.jsx
@@ -6,7 +6,26 @@ import './sign-in.css';
 export function Signin(){
     return (
         <main className="container">
-            <div>sign in here</div>
-        </main>
+        <div id="outerBox">
+            <div className="sign-in">
+                <h2 class>Sign in to your account</h2>
+                <form className="form-control">
+                    <div className="row rw">
+                        <label for="username" className="form-label">Username: </label>
+                        <input type="text" id="username" className="form-control" placeholder="Enter username" />
+                    </div>
+                    <div className="row rw">
+                        <label for="password" className="form-label">Password: </label>
+                        <input type="password" id="password" className="form-control" placeholder="Enter password"/>                        </div>
+                    
+                    </form>
+                <div className="row">
+                    <button className="btn btn-primary" type="submit">Sign In</button>
+                    <a href="lost-password.html">I forgot my password</a>
+                </div>
+            </div> 
+            <p id="createLink">Don't have an account? Create one <a href="create-account.html">here</a></p>
+        </div>
+    </main>
     );
 }
