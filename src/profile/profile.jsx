bb1c673916105db1f78d54b3e4b7778a45e49321
erush4 <ethan.t.rushforth@gmail.com>
2024-11-29 14:59:17 -0800

    Add Profile Component
    profile added, working fine

--- src/profile/profile.jsx
+++ src/profile/profile.jsx
@@ -6,7 +6,50 @@ import './profile.css';
 export function Profile(){
     return (
         <main className="container">
-            <div>profile here</div>
+            <h1>Profile</h1>
+            <hr />
+            <h2>Info</h2>
+                <img src="basic-profile.png" width="100" height="100" id="profile"/>
+                <div> User email: <span>name@domain.tld</span></div>
+                <div><button type="button" className="btn btn-warning">Change Password</button></div>
+        <hr />
+            <h2>Settings</h2>
+            <form>
+                <div>Spots to display on map:</div>
+                <div className="setting">
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" name="Ylot"/>Y (Student)
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" name="Glot"/>G (Graduate)
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" name="Alot"/>A (Faculty)
+                        </label>
+                    </div>
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" name="Ulot"/>U (Free)
+                        </label>
+                    </div>
+                </div>
+                <div>Privacy:</div>
+                <div className="setting">
+                    <div className="form-check">
+                        <label className="form-check-label">
+                            <input type="checkbox" className="form-check-input" name="anonymous"/>Submit surveys anonymously
+                        </label>
+                    </div>
+                </div>
+            </form>
+            <div><button type="button" className="btn btn-secondary" >Apply</button></div>
+        <hr />
+            <div><button type="button" className="btn btn-danger" >Sign Out</button></div>
         </main>
     );
 }
