4dc1a6d8694ee6ebd3bfd2499a6aece18900e302
erush4 <ethan.t.rushforth@gmail.com>
2024-12-02 14:51:21 -0700

    Debug Signin
    signin button now works
    no account creation yet

--- src/sign-in/unauthenticated.jsx
+++ src/sign-in/unauthenticated.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './unauthenticated.css';
-import { AuthState } from './authState';
 import Button from 'react-bootstrap/Button'
 
 export function Unauthenticated(props){
@@ -27,15 +26,15 @@ export function Unauthenticated(props){
                 <form className="form-control">
                     <div className="row rw">
                         <label htmlFor="username" className="form-label">Username: </label>
-                        <input type="text" id="username" className="form-control" placeholder="Enter username" />
+                        <input type="text" id="username" className="form-control" onChange={(c) => setUserName(c.target.value)} placeholder="Enter username" />
                     </div>
                     <div className="row rw">
                         <label htmlFor="password" className="form-label">Password: </label>
-                        <input type="password" id="password" className="form-control" placeholder="Enter password"/>                        </div>
+                        <input type="password" id="password" className="form-control" onChange={(c) => setPassword(c.target.value)} placeholder="Enter password"/>                        </div>
                     
                     </form>
                 <div className="row">
-                    <button className="btn btn-primary" type="submit">Sign In</button>
+                    <Button variant='primary' onClick={() => loginUser()} disabled={!userName || !password}>Sign In</Button>
                     <a href="lost-password.html">I forgot my password</a>
                 </div>
             </div> 
