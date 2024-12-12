aa493e728d84e13ac977f4417ec5b832261d1e28
erush4 <ethan.t.rushforth@gmail.com>
2024-12-12 00:31:55 -0700

    Copy necessary Simon code
    why fix what ain't broke
    except I changed some of the variable names because I don't use emails

--- service/index.js
+++ service/index.js
@@ -16,48 +16,59 @@ app.use(cookieParser());
 // Serve up the applications static content
 app.use(express.static('public'));
 
-  app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-  });
-let users = {};
-let data = [];
-let heatmap = [];
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set('trust proxy', true);
 
-var apiRouter = express.Router();
-app.use('/api', apiRouter);
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
 
-// Create a new user
+// CreateAuth token for a new user
 apiRouter.post('/auth/create', async (req, res) => {
-    const existingUser = users[req.body.username];
-    if (existingUser) {
-        res.status(409).send({ msg: 'Username already exists!' });
+    if (await DB.getUser(req.body.username)) {
+      res.status(409).send({ msg: 'Existing user' });
     } else {
-        const newUser = { username: req.body.username, password: req.body.password, anonymous: false, token: uuid.v4() };
-        users[newUser.username] = newUser;
-        res.send({ token: newUser.token });
+      const user = await DB.createUser(req.body.username, req.body.password);
+  
+      // Set the cookie
+      setAuthCookie(res, user.token);
+  
+      res.send({
+        id: user._id,
+      });
     }
-});
+  });
 
-// Login user
-apiRouter.post('/auth/signin', async (req, res) => {
-    const user = users[req.body.username];
+  // GetAuth token for the provided credentials
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await DB.getUser(req.body.username);
     if (user) {
-        if (req.body.password === user.password) {
-            user.token = uuid.v4();
-            res.send({ token: user.token, anonymous: user.anonymous });
-            return;
-        }
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        setAuthCookie(res, user.token);
+        res.send({ id: user._id });
+        return;
+      }
     }
-    res.status(401).send({ msg: 'Incorrect username or password' });
-});
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
 
-// Logout user
-apiRouter.delete('/auth/signout', (req, res) => {
-    const user = Object.values(users).find((u) => u.token === req.body.token);
-    if (user) {
-        delete user.token;
-    }
+// DeleteAuth token if stored in cookie
+apiRouter.delete('/auth/logout', (_req, res) => {
+    res.clearCookie(authCookieName);
     res.status(204).end();
+  });
+
+  //secureApiRouter verifies credentials for endpoints
+const secureApiRouter = express.Router();
+apiRouter.use(secureApiRouter);
+
+secureApiRouter.use(async (req, res, next) => {
+  const authToken = req.cookies[authCookieName];
+  const user = await DB.getUserByToken(authToken);
+  if (user) {
+    next();
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
 });
 
 // Get datapoints
@@ -114,6 +125,78 @@ apiRouter.post('/settings/anon', authenticateToken, (req, res) => {
     res.status(204).end();
 });
 
+// Default error handler
+app.use(function (err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+  });
+
+  // Return the application's default page if the path is unknown
 app.use((_req, res) => {
     res.sendFile('index.html', { root: 'public' });
   });
+  
+  // setAuthCookie in the HTTP response
+  function setAuthCookie(res, authToken) {
+    res.cookie(authCookieName, authToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: 'strict',
+    });
+  }
+  app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+  });
+
+// Get datapoints
+apiRouter.get('/data', (_req, res) => {
+    console.log('called', heatmap)
+    res.send(heatmap);
+});
+
+// Add datapoints
+apiRouter.post('/datapoint', (req, res) => {
+    console.log('called')
+    let point = req.body;
+    heatPoint = {
+        location: point.location,
+        weight: point.value
+    };
+    for (let thing of data) {
+        if (thing.location === heatPoint.location) {
+            thing.value = heatPoint.weight;
+            thing.username = point.username;
+            return data;
+        }
+    }
+    data.push(point);
+    heatmap.push(heatPoint);
+    res.send(heatmap);
+    if (data.length > 2000) {
+        data.length = 2000;
+    }
+    if (heatmap.length > 200) {
+        heatmap.length = 200;
+    }
+});
+
+// Token authentication middleware
+function authenticateToken(req, res, next) {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) return res.status(401).send({ msg: 'No token provided' });
+
+    const token = authHeader.split(' ')[1]; // Extract the token from the "Bearer <token>" format
+    if (!token) return res.status(401).send({ msg: 'No token provided' });
+
+    const user = Object.values(users).find(user => user.token === token);
+    if (!user) return res.status(403).send({ msg: 'Invalid token' });
+
+    req.user = user;
+    next();
+}
+
+// Set anonymous (protected route)
+apiRouter.post('/settings/anon', authenticateToken, (req, res) => {
+    const user = req.user;
+    user.anonymous = req.body.anonymous;
+    res.status(204).end();
+});
