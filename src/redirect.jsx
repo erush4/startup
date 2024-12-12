b77c85874f581816de07c9cfa645134bac274647
erush4 <ethan.t.rushforth@gmail.com>
2024-12-12 00:23:21 -0700

    Added forgotten

---
+++ src/redirect.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+import { AuthState } from "./sign-in/authState";
+export function Redirect(props) {
+    const navigate = useNavigate()
+    useEffect(() => {
+        navigate('/Signin');
+    }, [navigate]);
+    return (<div>loading...</div>);
+}
