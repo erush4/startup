42cfaefd308c06efa165ba241f291188c322f2a1
erush4 <ethan.t.rushforth@gmail.com>
2024-12-14 00:48:49 -0700

    Fix websocket not opening
    vite.config.js was not specifying /ws

--- service/peerProxy.js
+++ service/peerProxy.js
@@ -2,11 +2,13 @@ const { WebSocketServer } = require('ws');
 const uuid = require('uuid');
 
 function peerProxy(httpServer) {
+    console.log('peerProxy called');
   // Create a websocket object
   const wss = new WebSocketServer({ noServer: true });
-
+  console.log(wss)
   // Handle the protocol upgrade from HTTP to WebSocket
   httpServer.on('upgrade', (request, socket, head) => {
+    console.log('updating http');
     wss.handleUpgrade(request, socket, head, function done(ws) {
       wss.emit('connection', ws, request);
     });
@@ -16,11 +18,14 @@ function peerProxy(httpServer) {
   let connections = [];
 
   wss.on('connection', (ws) => {
+    console.log('connection called')
     const connection = { id: uuid.v4(), alive: true, ws: ws };
     connections.push(connection);
+    console.log('connection established')
 
     // Forward messages to everyone INCLUDING the sender
     ws.on('message', function message(data) {
+        console.log('message');
       connections.forEach((c) => {
           c.ws.send(data);
       });
@@ -28,6 +33,7 @@ function peerProxy(httpServer) {
 
     // Remove the closed connection so we don't try to forward anymore
     ws.on('close', () => {
+        console.log('closed')
       const pos = connections.findIndex((o, i) => o.id === connection.id);
 
       if (pos >= 0) {
@@ -37,12 +43,14 @@ function peerProxy(httpServer) {
 
     // Respond to pong messages by marking the connection alive
     ws.on('pong', () => {
+        console.log('pong')
       connection.alive = true;
     });
   });
 
   // Keep active connections alive
   setInterval(() => {
+    console.log('interval')
     connections.forEach((c) => {
       // Kill any connection that didn't respond to the ping last time
       if (!c.alive) {
--- src/map/dataDistributor.js
+++ src/map/dataDistributor.js
@@ -10,34 +10,49 @@ class DataDistributor {
     data=[]
     
     constructor() {
+        console.log("constructing")
         let port = window.location.port;
+        console.log(port)
         const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        console.log(protocol)
+        console.log('thing',`${protocol}://${window.location.hostname}:${port}/ws`);
         this.socket = new WebSocket(`${protocol}://${window.location.hostname}:${port}/ws`);
+        console.log(this.socket)
+
+        this.socket.onopen = () => {
+            console.log('webSocket opened')
+        }
+
+        this.socket.onclose = () => {
+            console.log("websocket closed")
+        }
+
+        this.socket.onerror = (error) => { console.error('WebSocket error', error); };
 
         this.socket.onmessage = async (msg) => {
           try {
             const datum = JSON.parse(await msg.data());
-            this.receiveEvent(datum);
+            this.data.push(datum);
           } catch {}
         };
       }
 
       broadcastDatum(datum){
+        console.log("broadcasting")
         if (this.socket.readyState === WebSocket.OPEN) {
             this.socket.send(JSON.stringify(datum))
+            console.log('rocket')
         } else {
+            console.log("sending")
             this.socket.addEventListener('open', () => {
                 this.socket.send(JSON.stringify(datum))
+                console.log("sent")
             })
         }
         
       }
-
-      receiveEvent(datum) {
-        this.data.push(datum);
-        };
-      }
-
+    }
 
 const Distributor = new DataDistributor();
+console.log(Distributor)
 export {DataPoint, Distributor};
